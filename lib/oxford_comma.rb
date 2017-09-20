f82ebd03a3804196eb8eeafc7dc7f4f38621d134
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-20 19:19:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,16 +3,16 @@ require 'pry'
 def oxford_comma(array)
 
   if array.length <2
-     return "#{array[0]}"
+    "#{array[0]}"
 
   elsif array.length == 2
-    return "#{array[0]} and #{array[1]}"
+     "#{array[0]} and #{array[1]}"
 
   else
     new = array[0.. -2].join(", ")
     last_word = array[-1]
     binding.pry
-    return "#{new}, and #{last_word}"
+    "#{new}, and #{last_word}"
 
 
    end
@@ -20,4 +20,5 @@ def oxford_comma(array)
 end
 
 x = oxford_comma(["miriam", "dakota", "ashley"])
+puts oxford_comma(["miriam", "dakota", "ashley"])
 # separate the first elements final element, create new array ( and element)
