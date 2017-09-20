c93e422208cedd705775bbd35fcb991263a7c353
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-20 19:00:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
+
+
 def oxford_comma(array)
 
+  if array.length <2
+     return "#{array[0]}"
+
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+
+  else
+    new = []
+    last_word= []
+    new = array[0.. -2].join(", ")
+    last_word = array[-1]
+    return "#{new} + and +#{last_word}"
+    new[-1].push(" and ")
+
+   end
+
 end
+# separate the first elements final element, create new array ( and element)
