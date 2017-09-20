486f9da4b445ae2e43a64bef03b9ff8a9ef64658
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-20 19:03:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ def oxford_comma(array)
     last_word= []
     new = array[0.. -2].join(", ")
     last_word = array[-1]
-    return "#{new,} and #{last_word}"
+    return "#{new}, and #{last_word}"
     
 
    end
